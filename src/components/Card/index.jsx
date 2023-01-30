924ca997c99da9243e9c69d43ac86b4d42ee3be9
Enderson Lynhares <enderson.linhares20@gmail.com>
2023-01-30 20:55:18 -0300

    animando os card com uma biblioteca

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,14 +1,15 @@
 import styled from "./index.module.css";
+import Tilt from "react-parallax-tilt";
 
 export const Card = ({ style, src, name, ...props }) => {
- 
   return (
+    <Tilt>
       <div style={style} className={styled.card} {...props}>
         <div className={styled.imageCard}>
           <img src={src} alt="" />
         </div>
         <h4>{name}</h4>
-        
       </div>
+    </Tilt>
   );
 };
